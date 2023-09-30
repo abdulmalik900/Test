4da82d28031f31d0cf37f6ac879c5ce746a42a4b
abdulmalik900 <amalik90090@gmail.com>
2023-09-30 15:56:50 +0500

    ok

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,57 +1,42 @@
-'use client'
-import { Carousel } from 'flowbite-react';
-import Image from 'next/image';
+"use client";
+import { Carousel } from "flowbite-react";
+import Image from "next/image";
 
 const Hero = () => {
   return (
     <>
-     
-     <>
-         <div className='h-[100vh] w-full bg-gray-400'>
-         <Carousel onSlideChange={slideIdx=>console.log(slideIdx)}>
-            <div className='sm:h-full h-auto w-full'>
-                <Image 
-                  src="/image/slide1.jpg"
-                  alt="Slide 1"
-                  fill
-                  
-                  style={{objectFit:"cover"}}
-                />
-            </div>
-        
-            <div className='sm:h-full h-auto w-full '>
-                <Image 
-                  src="/image/slide2.jpg"
-                  alt="Slide 2"
-                  fill
-                 
-                  style={{objectFit:"cover"}}
-                />
-            </div>
-        
-            <div className='sm:h-full h-auto w-full'>
-                <Image 
-                  src="/image/slide3.jpg"
-                  alt="Slide 3"
-                  fill
-                  
-                  style={{objectFit:"cover"}}
-                />
-            </div>
-  </Carousel>
-</div>
+      <div className="h-[100vh] w-full bg-gray-400">
+        <Carousel onSlideChange={(slideIdx) => console.log(slideIdx)}>
+          <div className="sm:h-full h-[100vh] w-full">
+            <Image
+              src="/image/slide1.jpg"
+              alt="Slide 1"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
 
+          <div className="sm:h-full h-auto w-full ">
+            <Image
+              src="/image/slide2.jpg"
+              alt="Slide 2"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
 
-   </>
-
-    
+          <div className="sm:h-full h-auto w-full">
+            <Image
+              src="/image/slide3.jpg"
+              alt="Slide 3"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+        </Carousel>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Hero;
-
-
-
-
-
