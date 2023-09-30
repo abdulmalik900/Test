c91ecde92a53bdcfe752476902c2a636df6ae7dc
abdulmalik900 <amalik90090@gmail.com>
2023-09-30 16:30:04 +0500

    Responsiveness Issue

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,7 @@ import { Carousel } from "flowbite-react";
 import Image from "next/image";
 
 const Hero = () => {
-  return (
+  return (                
     <>
       <div className="h-[100vh] w-full bg-gray-400">
         <Carousel onSlideChange={(slideIdx) => console.log(slideIdx)}>
@@ -13,24 +13,50 @@ const Hero = () => {
               alt="Slide 1"
               fill
               style={{ objectFit: "cover" }}
+              className="hidden md:block"
             />
+            <Image
+              src="/image/mobile1.png"
+              alt="Slide 1"
+              fill
+              style={{ objectFit: "cover" }}
+              className="flex md:hidden z-50"
+            />
+
           </div>
 
-          <div className="sm:h-full h-auto w-full ">
+          <div className="sm:h-full h-[100vh] w-full ">
             <Image
               src="/image/slide2.jpg"
               alt="Slide 2"
               fill
               style={{ objectFit: "cover" }}
+              className="hidden md:block"
+            />
+              <Image
+              src="/image/mobile2.png"
+              alt="Slide 1"
+              fill
+              style={{ objectFit: "cover" }}
+              className="flex md:hidden z-50"
             />
           </div>
 
-          <div className="sm:h-full h-auto w-full">
+          <div className="sm:h-full h-[100vh] w-full">
             <Image
               src="/image/slide3.jpg"
               alt="Slide 3"
               fill
               style={{ objectFit: "cover" }}
+              className="hidden md:block"
+            />
+
+<Image
+              src="/image/mobile3.png"
+              alt="Slide 1"
+              fill
+              style={{ objectFit: "cover" }}
+              className="flex md:hidden z-50"
             />
           </div>
         </Carousel>
